2c5cb3b84920e2176dbd0df724980d0884acd617
Guy Cheung <guy.xtafcusqt@gmail.com>
2018-08-07 10:06:54 +0800

    add nbut include

--- nbuser/nbuser.go
+++ nbuser/nbuser.go
@@ -3,6 +3,7 @@ package nbuser
 import (
 	"github.com/NoneBorder/bee-modules/nbuser/nbum"
 	"github.com/NoneBorder/bee-modules/nbuser/nbur"
+	_ "github.com/NoneBorder/bee-modules/nbuser/nbut"
 )
 
 func Init(dbAlias, dbTablePrefix string) {
